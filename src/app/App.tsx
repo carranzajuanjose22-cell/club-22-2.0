315893124262fa7c695c2281cba1d832479a13f8
carranzajuanjose22-cell <carranza.juanjose22@gmail.com>
2026-04-17 02:14:34 -0300

    Ajuste de rutas raiz

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import { POSProvider } from "./context/POSContext";
 import { AppRoutes } from "./routes";
-import "../styles/main.css"; 
+// import "../styles/main.css"; // La comentamos con // para que no tire error
 
 function App() {
   return (
